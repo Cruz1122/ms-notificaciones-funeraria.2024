e17130322986d689d6eb5042c582961e655ed053
Jhonder18 <jhon.patino29550@ucaldas.edu.co>
2024-04-20 17:53:56 -0500

    Creacion de las rutas Email-2fa y SMS-2fa

--- server.py
+++ server.py
@@ -15,8 +15,29 @@ def home():
 
 @app.route("/sms", methods=["POST"])
 def sms():
-    destination = request.form["destination"]
-    message = request.form["message"]
+    data = request.get_json()
+    destination = data["destination"]
+    message = data["message"]
+    print(destination)
+    print(message) 
+    # Create an SNS client
+    client = boto3.client(
+        "sns",
+        aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
+        region_name=os.environ.get("AWS_REGION"),
+    )
+
+    # Send your sms message.
+    client.publish(PhoneNumber=destination, Message=message)
+    return "OK"
+
+@app.route("/sms-2fa", methods=["POST"])
+def sms_2fa():
+    data = request.get_json()
+    name = data["name"]
+    destination = data["destination"]
+    message = "Hola, "+ name+". \n" + data["message"]
     print(destination)
     print(message)
     # Create an SNS client
@@ -35,9 +56,10 @@ def sms():
 # based on the code above, build the email api method using AWS SES
 @app.route("/email", methods=["POST"])
 def email():
-    destination = request.form["destination"]
-    message = request.form["message"]
-    subject = request.form["subject"]
+    data = request.get_json()
+    destination = data["destination"]
+    message = data["message"]
+    subject = data["subject"]
     # Create an SES client
     client = boto3.client(
         "ses",
@@ -68,6 +90,43 @@ def email():
     )
     return response
 
+@app.route("/email-2fa", methods=["POST"])
+def email_2fa():
+    data = request.get_json()
+    destination = data["destination"]
+    name = data["name"]
+    message = data["message"]
+    subject = data["subject"]
+    
+    # Create an SES client
+    client = boto3.client(
+        "ses",
+        aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
+        region_name=os.environ.get("AWS_REGION"),
+    )
+    # send the email message using the client
+    response = client.send_email(
+        Destination={
+            "ToAddresses": [
+                destination,
+            ],
+        },
+        Message={
+            "Body": {
+                "Text": {
+                    "Charset": "UTF-8",
+                    "Data": "Hola, "+ name+". \n" + message,
+                },
+            },
+            "Subject": {
+                "Charset": "UTF-8",
+                "Data": subject,
+            },
+        },
+        Source="funerariadigitaluc@gmail.com",
+    )
+    return response
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
